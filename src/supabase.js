2580179e147bb02bdd467fd4b9607d9545636617
Isaac <wavearena5@gmail.com>
2026-05-09 22:06:08 +0100

    Update to new Supabase publishable key

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://bqwsosjmyhrxujtjcmqi.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJxd3Nvc2pteWhyeHVqdGpjbXFpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc1NzMwNDcsImV4cCI6MjA5MzE0OTA0N30.DbKVABF5PnQkVP7r1eg7vs2L1R-DdJJ_B-_0bhwguSg'
+const supabaseKey = 'sb_publishable_zi6P_Swt8plSOISQSdc9RA_GINGDCfv'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
